f6772e10801c48438b38f9d01d2edbbfa24d551e
Xdellta <66367896+Xdellta@users.noreply.github.com>
2021-01-09 19:27:51 +0100

    Update main.cpp

--- zad 18/main.cpp
+++ zad 18/main.cpp
@@ -6,21 +6,27 @@
 #include <iostream>
 #include <cstdlib>
 #include <time.h>
-#include <locale.h>
-#include <cstdlib>
-
 using namespace std;
 
 int main()
 {
- unsigned int i=0;
- for (;i<=10;++i)
- {
-    cout <<"Wykonuje sie ";
-    cout <<i+1<<" raz"<<endl;
- }
- cout <<endl<<"Nacisnij ENTER aby zakonczyc"<<endl;
- system("PAUSE >nul");
+     //Nykiel Sebastian 2AG Program 18
+    setlocale(LC_CTYPE, "Polish");
+    srand(time(0));
+
+    int orly = 0,reszki = 0,kostka1,kostka2,rzut;
+
+    while(kostka1 != kostka2)
+    {
+        rzut++;
+        kostka1 = rand() % 6 + 1;
+        kostka2 = rand() % 6 + 1;
+
+        cout << "\t  RZUT  nr." << rzut << endl;
+        cout << " Kostka 1 :" << kostka1 << endl;
+        cout << " Kostka 2 :" << kostka2 << endl << endl;
+    }
+    cout << "\n \t Ilość rzutów: " << rzut << endl << endl;
 
     return 0;
 }
